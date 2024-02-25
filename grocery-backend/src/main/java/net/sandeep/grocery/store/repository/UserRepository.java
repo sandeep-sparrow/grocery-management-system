535525b361328f89c2f157165ef81c792646bce9
sandeep-simplify <sandeep@simplifymoney.in>
2024-02-25 23:21:15 +0530

    removed unsed import from spring boot backend

--- grocery-backend/src/main/java/net/sandeep/grocery/store/repository/UserRepository.java
+++ grocery-backend/src/main/java/net/sandeep/grocery/store/repository/UserRepository.java
@@ -3,7 +3,6 @@ package net.sandeep.grocery.store.repository;
 import net.sandeep.grocery.store.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
